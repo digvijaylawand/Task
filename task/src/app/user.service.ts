25c2971a40ed582d2a59b10c18676dc1a0d066d3
digvijaylawand <67827645+digvijaylawand@users.noreply.github.com>
2021-07-07 12:29:36 +0530

    changing port for default json server

--- task/src/app/user.service.ts
+++ task/src/app/user.service.ts
@@ -3,7 +3,7 @@ import { HttpClient , HttpHeaders} from "@angular/common/http";
 import { User } from "./user";
 import { Observable } from 'rxjs';
 
-const localUrl = 'http://localhost:8000';
+const localUrl = 'http://localhost:3000';
 const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type':  'application/json',
